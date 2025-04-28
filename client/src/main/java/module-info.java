fbc896162c1bbf97066048ea9204b5ae2c919f55
MeoMeoCat:) <71182846+renyuan-fei@users.noreply.github.com>
2025-04-28 23:13:27 +1000

    Initial commit

---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.whiteboard.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.whiteboard.client to javafx.fxml;
+    exports org.whiteboard.client;
+}
